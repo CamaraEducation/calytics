43ed671fe7d2212fbb4eb359f3ed11a41af946c9
Abdulbasit Rubeiyya <abdulbasitsultan4@gmail.com>
2022-05-24 21:14:07 +0300

    template

---
+++ views/layout/home.blade.php
@@ -0,0 +1,23 @@
+@extends('layout.app')
+@yield('title')
+@section('menu', '')
+@section('submenu', '')
+@section('content')
+			
+	<div class="content-wrapper">
+
+		<div class="row gutters">
+			<div class="col-xl-12">
+				<!-- Card start -->
+				<div class="card">
+					<div class="card-header">
+						<div class="card-title">Card Header</div>
+					</div>
+					<div class="card-body">
+					</div>
+				</div>
+			</div>
+		</div>
+
+	</div>
+@endsection
