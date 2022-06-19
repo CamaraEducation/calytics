7328066492ea19cf6619777292dfe81d26ad780b
Abdulbasit Rubeiyya <abdulbasitsultan4@gmail.com>
2022-06-19 14:10:51 +0300

    utilities ie, time, spreadsheet reader ec

--- controls/utilities/icons.php
+++ controls/utilities/icons.php
@@ -10,5 +10,20 @@ class IconsController{
             return self::$path.'default.png';
         }
     }
+
+    public static function doc($file){
+        $ext = pathinfo($file, PATHINFO_EXTENSION);
+        $sql = "SELECT path FROM icons WHERE tags LIKE '%$ext%' LIMIT 1";        
+        if(!empty($ext)){
+            if(mysqli_num_rows(mysqli_query(conn(), $sql)) > 0){
+                $res = mysqli_fetch_assoc(mysqli_query(conn(), $sql));
+                return self::$path.$res['path'];
+            }else{
+                return self::$path.'default.png';
+            }
+        }else{
+            return self::$path.'icons8-file-explorer-50.png';
+        }
+    }
 }
 ?>
