0771e27caede974208e5ac9fd059f153001176b7
Abdulbasit Rubeiyya <abdulbasitsultan4@gmail.com>
2022-06-19 14:11:50 +0300

    manic time modals

---
+++ views/modals/manic/file_overview.blade.php
@@ -0,0 +1,49 @@
+<div class="row gutter">
+    <div class="col-md-12">
+        <div class="card">
+            <canvas id="sc_file_stat"></canvas>
+        </div>
+    </div>
+
+    <div class="col-md-12">
+        <div class="card" style="background: lightgray;  border-radius: 10px; }">
+            <div class="card-body">
+                <ul class="statistics shorts-fsum">
+
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
+
+<script>
+    $(document).ready(function() {
+        var app_stats = {!! json_encode(ManicStatsController::manic_sfile_stat($id, $file)) !!};
+        sc_file_stat(app_stats['date'], app_stats['duration']);
+
+        /*
+         * app_stats['duration'] is array
+         * generate a list of total, average, max and min from app_stats['duration']
+         * append list to shorts
+        */
+        var shorts = $('.shorts-fsum');
+        var total = 0; var average = 0; var max = 0; var min = 0; var count = 0;
+        for (var i = 0; i < app_stats['duration'].length; i++) {
+            total += app_stats['duration'][i];
+            count++;
+            if (app_stats['duration'][i] > max) {
+                max = app_stats['duration'][i];
+            }
+            if (app_stats['duration'][i] < min) {
+                min = app_stats['duration'][i];
+            }
+        }
+        average = total / count;
+        var list = ''+
+            '<li><span class="stat-icon"><i class="icon-cloud"></i></span> Total<br> ' + timeCalc(total*3600) + '</li>' +
+            '<li><span class="stat-icon"><i class="icon-cloud-rain"></i></span> Average<br> ' + timeCalc(average*3600) + '</li>' +
+            '<li><span class="stat-icon"><i class="icon-cloud-lightning"></i></span> Max<br> ' + timeCalc(max*3600) + '</li>' +
+            '<li><span class="stat-icon"><i class="icon-cloud-snow"></i></span> Min<br> ' + timeCalc(min*3600) + '</li>';
+        shorts.html(list);
+    });
+</script>
