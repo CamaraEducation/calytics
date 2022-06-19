fd43bcabf96aaa19247b6ec0d3a7c9ccd236f26f
Abdulbasit Rubeiyya <abdulbasitsultan4@gmail.com>
2022-06-19 14:12:54 +0300

    landing page

---
+++ views/public/landing.blade.php
@@ -0,0 +1,155 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<!-- meta tags -->
+		<meta charset="utf-8">
+		<meta name="keywords" content="bootstrap 5, premium, multipurpose, sass, rtl, data, science, research, analytics" />
+		<meta name="description" content="Data Science & Analytics Bootstrap 5 Template" />
+		<meta name="author" content="www.themeht.com" />
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<!-- Title -->
+		<title>CALYTICS</title>
+		<!-- Favicon Icon -->
+		<link rel="shortcut icon" href="/assets/img/fav.png" />
+		<link href="/assets/landing/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+		<link href="/assets/landing/https://fonts.googleapis.com/css2?family=Be+Vietnam+Pro:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500;1,600;1,700;1,800;1,900&amp;display=swap" rel="stylesheet">
+		<link href="/assets/landing/https://fonts.googleapis.com/css2?family=Outfit:wght@300;400;500;600;700;800;900&amp;display=swap" rel="stylesheet">
+		<link href="/assets/landing/css/animate.css" rel="stylesheet" type="text/css" />
+		<link href="/assets/landing/css/line-awesome.min.css" rel="stylesheet" type="text/css" />
+		<link href="/assets/landing/css/magnific-popup.css" rel="stylesheet" type="text/css" />
+		<link href="/assets/landing/css/owl.carousel.css" rel="stylesheet" type="text/css" />
+		<link href="/assets/landing/css/base.css" rel="stylesheet" type="text/css" />
+		<link href="/assets/landing/css/shortcodes.css" rel="stylesheet" type="text/css" />
+		<link href="/assets/landing/css/spacing.css" rel="stylesheet" type="text/css" />
+		<link href="/assets/landing/css/style.css" rel="stylesheet" type="text/css" />
+		<link href="/assets/landing/css/responsive.css" rel="stylesheet" type="text/css" />
+	</head>
+	<body style="overflow: hidden">
+		<div class="page-wrapper">
+		<div id="ht-preloader">
+			<div class="clear-loader d-flex align-items-center justify-content-center">
+				<div class="loader"> <span>Calytics</span>
+				</div>
+			</div>
+		</div>
+		<header id="site-header" class="header header-2">
+			<div id="header-wrap">
+				<div class="container">
+					<div class="row">
+						<div class="col">
+							<nav class="navbar navbar-expand-lg">
+								<a class="navbar-brand logo mb-0" href="/assets/landing/index.html">
+								<img class="img-fluid" src="https://camara.org/wp-content/uploads/2020/10/camara_logo_left_white-1.png" alt="">
+								</a>
+								<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation"> <span></span>
+								<span></span>
+								<span></span>
+								</button>
+								<div class="collapse navbar-collapse" id="navbarNav">
+								</div>
+								<div class="right-nav align-items-center d-flex justify-content-end">
+									<a href="/assets/landing/#" class="ht-nav-toggle ms-md-4 ms-3"><span></span></a>
+								</div>
+							</nav>
+						</div>
+					</div>
+				</div>
+			</div>
+		</header>
+		<nav id="ht-main-nav">
+			<a href="/assets/landing/#" class="ht-nav-toggle active"><span></span></a>
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12">
+						<h3>What is Calytics?</h3>
+						<p class="mb-5">Calytics stands for Camara Analytics Dasboards, it is a utility tool by camara to track and analyse the usage data for all devices and systems deployed acrosss the hubs</p>
+						<div class="form-info">
+							<h4 class="title">Contact info</h4>
+							<ul class="media-icon list-unstyled font-w-5">
+								<li>
+									<i class="las la-map-pin"></i>
+									<p class="mb-0">Camara Education Tanzania, <br>
+										Block 3, Plot No. 47, Engaruka St, <br>
+										Kurasini, Dar Es Salaam
+									</p>
+								</li>
+								<li><i class="las la-envelope-open-text"></i>  <a href="/assets/landing/mailto:themeht23@gmail.com">tanzania@camara.org</a>
+								</li>
+								<li><i class="las la-phone-volume"></i>  
+									<a href="/assets/landing/tel:+912345678900">+255 758 396 600</a>
+								</li>
+							</ul>
+						</div>
+						<div class="social-icons social-colored mt-5">
+							<ul class="list-inline">
+								<li class="mb-2 social-facebook"><a href="/assets/landing/#"><i class="lab la-facebook-f"></i></a>
+								</li>
+								<li class="mb-2 social-twitter"><a href="/assets/landing/#"><i class="lab la-twitter"></i></a>
+								</li>
+								<li class="mb-2 social-linkedin"><a href="/assets/landing/#"><i class="lab la-linkedin-in"></i></a>
+								</li>
+								<li class="mb-2 social-gplus"><a href="/assets/landing/#"><i class="lab la-google-plus-g"></i></a>
+								</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</nav>
+		<section class="hero-banner banner-2 position-relative overflow-hidden bg-dark custom-py-1" style="height:100vh">
+			<div class="container">
+				<div class="row align-items-center">
+					<div class="col-12 col-lg-6 order-lg-1 mb-8 mb-lg-0 z-index-1 text-end banner">
+						<div class="banner-morph "></div>
+						<!-- Image -->
+						<img class="img-fluid topBottom" src="/assets/landing/images/02.png" alt="">
+					</div>
+					<div class="col-12 col-lg-6">
+						<h6 class="px-3 py-2 text-white rounded d-inline-block mb-4 box-shadow" data-bg-color="rgba(255,255,255,0.1)">CALYTICS</h6>
+						<h1 class="mb-4 text-white">
+							<span style="color: white">Camara</span>
+							<span class="font-w-5">Analytics n Statistic</span> Tool.          
+						</h1>
+						<p class="lead mb-5 text-rgba"></p>
+						@if(isset($_SESSION['user']))
+    						<a href="/dashboard" class="btn btn-primary me-2">
+    						Dashboard
+    						</a>
+    					@else
+    						<a href="/login" class="btn btn-secondary me-2">
+    						Login
+    						</a>
+    					@endif
+					</div>
+				</div>
+			</div>
+		</section>
+		<div class="modal fade cart-modal" id="cartModal" tabindex="-1" role="dialog" aria-labelledby="ModalLabel" aria-hidden="true">
+		</div>
+		<!--back-to-top start-->
+		<div class="scroll-top"><a class="smoothscroll" href="/assets/landing/#top"><img class="img-fluid" src="/assets/landing/images/top-arrow.png" alt=""></a></div>
+		<!--back-to-top end-->
+		<!-- inject js start -->
+		<!--== jquery -->
+		<script src="/assets/landing/js/jquery.min.js"></script>
+		<!--== bootstrap -->
+		<script src="/assets/landing/js/bootstrap.bundle.min.js"></script>
+		<!--== modernizr -->
+		<script src="/assets/landing/js/modernizr.min.js"></script>
+		<!--== owl-carousel -->
+		<script src="/assets/landing/js/owl.carousel.min.js"></script> 
+		<!--== magnific-popup --> 
+		<script src="/assets/landing/js/jquery.magnific-popup.min.js"></script>
+		<!--== counter -->
+		<script src="/assets/landing/js/counter.js"></script> 
+		<!--== countdown -->
+		<script src="/assets/landing/js/jquery.countdown.min.js"></script>
+		<!--== skill bars -->
+		<script src="/assets/landing/js/skill.bars.jquery.js"></script> 
+		<!--== canvas -->
+		<script src="/assets/landing/js/canvas-js.js"></script>
+		<!--== theme-script -->
+		<script src="/assets/landing/js/theme-script.js"></script>
+		<!-- inject js end -->
+	</body>
+</html>
