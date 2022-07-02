bed39e257adc7ca30ac3768a9587c0565963e6bc
Abdulbasit Rubeiyya <abdulbasitsultan4@gmail.com>
2022-07-02 11:04:52 +0300

    portal action menu

--- views/layout/tabs/portal.blade.php
+++ views/layout/tabs/portal.blade.php
@@ -1,5 +1,28 @@
 <div class="tab-pane-header">
     Portal
 </div>
-
-<p class="form-control">under Construction</p>
+<div class="sidebarMenuScroll">
+    <div class="sidebar-menu">
+        <ul class="tile-menu">
+            <li>
+                <a href="/portal/dashboard"><i class="icon-laptop"></i> Dashboard</a>
+            </li>
+            <li>
+                <a href="/portal/report"><i class="icon-layers2"></i> Reports</a>
+            </li>
+            <li>
+                <a href="/portal/data/add"><i class="icon-arrow-up-circle"></i> Jobs</a>
+            </li>
+            <li>
+                <a href="/portal/data/apps"><i class="icon-box"></i> Applets</a>
+            </li>
+            <li>
+                <a href="/portal/data/schools"><i class="icon-location"></i> Schools</a>
+            </li>
+            
+            <li>
+                <a href="/portal/data/projects"><i class="icon-location"></i> Projects</a>
+            </li>
+        </ul>
+    </div>
+</div>
