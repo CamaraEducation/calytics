981016adb0ea318651925c57ec6aefc202301791
Abdulbasit Rubeiyya <abdulbasitsultan4@gmail.com>
2022-07-03 23:18:56 +0300

    space - tabs

--- views/portal/report.blade.php
+++ views/portal/report.blade.php
@@ -1,128 +1,136 @@
 @extends('layout.app')
-@section('menu', '')
+@section('menu', 'portal')
 @section('submenu', 'portal')
 @section('title', 'Portal Brief')
 @section('styles')
-
+<link rel="stylesheet" href="/assets/vendor/daterange/daterange.css">
 @endsection
 @section('content')
 
-    <div class="content-wrapper" id="print">
-        <div class="row gutters">
-            <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+	<div class="content-wrapper" id="print">
+		<div class="row gutters">
+			<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
 
-                <div class="profile-header">
-                    <h1>Portal Brief</h1>
-                    <div class="profile-header-content">
-                        <div class="profile-header-tiles">
-                            <div class="row gutters">
-                                <div class="col-xl-4 col-lg-4 col-md-4 col-sm-6 col-12">
-                                    <div class="profile-tile">
-                                        <span class="icon">
-                                            <i class="icon-map-pin"></i>
-                                        </span>
-                                        <h6>
-                                            <span>{{PortalController::unique_schools()}}</span> 
-                                            - Total Schools
-                                        </h6>
-                                    </div>
-                                </div>
-                                <div class="col-xl-4 col-lg-4 col-md-4 col-sm-6 col-12">
-                                    <div class="profile-tile">
-                                        <span class="icon">
-                                            <i class="icon-server"></i>
-                                        </span>
-                                        <h6>
-                                            <span>{{PortalController::unique_sessions()}}</span> 
-                                            - Unique Sessions
-                                        </h6>
-                                    </div>
-                                </div>
-                                <div class="col-xl-4 col-lg-4 col-md-4 col-sm-12 col-12">
-                                    <div class="profile-tile">
-                                        <span class="icon">
-                                            <i class="icon-clock1"></i>
-                                        </span>
-                                        <h6>
-                                            <span>{{PortalController::live_time()}}</span> 
-                                            - Live Time
-                                        </h6>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="profile-avatar-tile">
-                            <button class="btn btn-primary container-fluid text-bold" data-bs-toggle="modal" data-bs-target="#exampleModalFullScreen">Filter</button>
-                            <div class="xs-space"></div>
-                            <button class="btn btn-warning container-fluid text-bold" onclick="printDiv('print')">Print</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+				<div class="profile-header">
+					<h1>Portal Brief</h1>
+					<div class="profile-header-content">
+						<div class="profile-header-tiles">
+							<div class="row gutters">
+								<div class="col-xl-4 col-lg-4 col-md-4 col-sm-6 col-12">
+									<div class="profile-tile">
+										<span class="icon">
+											<i class="icon-map-pin"></i>
+										</span>
+										<h6>
+											<span>{{PortalController::unique_schools()}}</span> 
+											- Total Schools
+										</h6>
+									</div>
+								</div>
+								<div class="col-xl-4 col-lg-4 col-md-4 col-sm-6 col-12">
+									<div class="profile-tile">
+										<span class="icon">
+											<i class="icon-server"></i>
+										</span>
+										<h6>
+											<span>{{PortalController::unique_sessions()}}</span> 
+											- Unique Sessions
+										</h6>
+									</div>
+								</div>
+								<div class="col-xl-4 col-lg-4 col-md-4 col-sm-12 col-12">
+									<div class="profile-tile">
+										<span class="icon">
+											<i class="icon-clock1"></i>
+										</span>
+										<h6>
+											<span>{{PortalController::live_time()}}</span> 
+											- Live Time
+										</h6>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="profile-avatar-tile no-print">
+							<button class="btn btn-primary container-fluid text-bold" data-bs-toggle="modal" data-bs-target="#filterPortalReport" disabled>Filter</button>
+							<div class="xs-space"></div>
+							<button class="btn btn-warning container-fluid text-bold" id="btnConvert" disabled>Print</button>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="row gutters no-print" id="filterTile">
+			<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+				
+				<!-- Card start -->
+				<div class="card">
+					<div class="card-body">
+						
+						<!-- Row start -->
+						<div class="row gutters">
+							<div class="col-xl-8 col-lg-8 col-md-8 col-sm-8 col-12">
+
+								<!-- Field wrapper start -->
+								<div class="field-wrapper">
+									<div class="input-group">
+										<input type="text" class="form-control custom-daterange2" name="range" id="ranger">
+										<span class="input-group-text">
+											<i class="icon-calendar1"></i>
+										</span>
+									</div>
+									<div class="field-placeholder">Custom Date Range</div>
+								</div>
+								<!-- Field wrapper end -->
+
+							</div>
+
+							<div class="col-xl-4 col-lg-4 col-md-4 col-sm-4 col-12">
+
+								<!-- Field wrapper start -->
+								<div class="field-wrapper">
+									<button type="button" class="btn btn-secondary w-100" id="filter">Filter &nbsp; <i class="icon-filter"></i></button>
+								</div>
+								<!-- Field wrapper end -->
+
+							</div>
+						</div>
+						<!-- Row end -->
+
+					</div>
+				</div>
+			</div>
+		</div>
 
 		<div class="row gutters">
-			<div class="col-xl-6 col-lg-6 col-md-12 col-sm-12 col-12">
+			<div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12">
 				<div class="card">
 					<div class="card-header">
 						<div class="card-title">Portal Activity (Counts)</div>
-                        <ul class="nav nav-tabs" id="myTab" role="tablist">
-                            <li class="nav-item" role="presentation">
-                                <a class="nav-link active" id="activity-tab" data-bs-toggle="tab" href="#activity" role="tab" aria-controls="activity" aria-selected="true">By Months</a>
-                            </li>
-                            <li class="nav-item" role="presentation">
-                                <a class="nav-link" id="dt_activity-tab" data-bs-toggle="tab" href="#dt_activity" role="tab" aria-controls="dt_activity" aria-selected="false">By Dates</a>
-                            </li>
-                        </ul>
 					</div>
 					<div class="card-body">
-                        <div class="tab-content" id="myTabContent">
-                            <div class="tab-pane fade show active" id="activity" role="tabpanel">
-                                <canvas id="portal_activity">
+						<canvas id="dt_portal_activity">
 
-                                </canvas>
-                            </div>
-                            <div class="tab-pane fade" id="dt_activity" role="tabpanel">
-                                <canvas id="dt_portal_activity">
-
-                                </canvas>
-                            </div>
-                        </div>
+						</canvas>
 					</div>
 				</div>
 			</div>
 
-            <div class="col-xl-6 col-lg-6 col-md-12 col-sm-12 col-12">
+			<div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12">
 				<div class="card">
 					<div class="card-header">
 						<div class="card-title">Live Time (hrs)</div>
-                        <ul class="nav nav-tabs" id="myTab" role="tablist">
-                            <li class="nav-item" role="presentation">
-                                <a class="nav-link active" id="live-tab" data-bs-toggle="tab" href="#live" role="tab" aria-controls="live" aria-selected="true">By Months</a>
-                            </li>
-                            <li class="nav-item" role="presentation">
-                                <a class="nav-link" id="dt_live-tab" data-bs-toggle="tab" href="#dt_live" role="tab" aria-controls="dt_live" ari~a-selected="false">By dates</a>
-                            </li>
-                        </ul>
 					</div>
 					<div class="card-body">
-                        <div class="tab-content" id="myTabContent">
-                            <div class="tab-pane fade show active" id="live" role="tabpanel">
-                                <canvas id="portal_live_time">
+						<canvas id="dt_portal_live_time">
 
-                                </canvas>
-                            </div>
-                            <div class="tab-pane fade" id="dt_live" role="tabpanel">
-                                <canvas id="dt_portal_live_time">
-
-                                </canvas>
-                            </div>
-                        </div>
+						</canvas>
 					</div>
 				</div>
 			</div>
 
-            <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12">
+			<div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12">
 				<div class="card">
 					<div class="card-header">
 						<div class="card-title">Top Applets</div>
@@ -135,7 +143,7 @@
 				</div>
 			</div>
 
-            <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12">
+			<div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12">
 				<div class="card">
 					<div class="card-header">
 						<div class="card-title">Popular Documents</div>
@@ -148,7 +156,7 @@
 				</div>
 			</div>
 
-            <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+			<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
 				<div class="card">
 					<div class="card-header">
 						<div class="card-title">Popular Videos</div>
@@ -160,30 +168,37 @@
 					</div>
 				</div>
 			</div>
-        </div> 
+		</div> 
 	</div>
 
-    @include('modals.portal.filter_report')
+	<div id="previewImg"></div>
+
+	@include('modals.portal.filter_report')
 @endsection
 @section('scripts')
 @js('https://cdn.jsdelivr.net/npm/chart.js@3.7.1/dist/chart.min.js')
 @js('/assets/js/chart')
+
+<!-- Date Range JS -->
+<script src="/assets/vendor/daterange/daterange.js"></script>
+<script src="/assets/vendor/daterange/custom-daterange.js"></script>
+
+<!--script src="https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.10.1/html2pdf.bundle.min.js" integrity="sha512-GsLlZN/3F2ErC5ifS5QtgpiJtWd43JWSuIgh7mbzZ8zBps+dvLusV+eNQATqgA/HdeKFVgA5v3S/cIrLF7QnIg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script-->
 <script>
-	var rm_sess = {!! json_encode(PortalController::month_session_graph()) !!};
-    var rd_sess = {!! json_encode(PortalController::date_session_graph()) !!};
-    var rm_time = {!! json_encode(PortalController::month_liveTime_graph()) !!};
-    var rd_time = {!! json_encode(PortalController::date_liveTime_graph()) !!};
-    var applet  = {!! json_encode(PortalController::applets_stats(10)) !!};
-    var video   = {!! json_encode(PortalController::video_stats(10)) !!};
-    var doc     = {!! json_encode(PortalController::doc_stats(10)) !!};
-    
-	portal_month_sess(rm_sess);
-    portal_date_sess(rd_sess.date, rd_sess.sess);
-    portal_month_ltime(rm_time);
-    portal_date_ltime(rd_time.date, rd_time.live);
-    top_applets(applet.title, applet.activity);
-    top_videos(video.title, video.activity);
-    top_docs(doc.title, doc.activity);
+	var rd_sess = {!! json_encode(PortalController::date_session_graph()) !!};
+	var rd_time = {!! json_encode(PortalController::date_liveTime_graph()) !!};
+	var applet  = {!! json_encode(PortalController::applets_stats(10)) !!};
+	var video   = {!! json_encode(PortalController::video_stats(10)) !!};
+	var doc     = {!! json_encode(PortalController::doc_stats(10)) !!};
+	
+	portal_date_sess(rd_sess.date, rd_sess.sess);
+	portal_date_ltime(rd_time.date, rd_time.live);
+	top_applets(applet.title, applet.activity);
+	top_videos(video.title, video.activity);
+	top_docs(doc.title, doc.activity);
+
+	generate_pdf();
+	filter_report();
 
 </script>
 @endsection
