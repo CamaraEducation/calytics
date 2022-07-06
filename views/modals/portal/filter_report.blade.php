9ac300533a01f6ebac7739c688f11523b2c42161
Abdulbasit Rubeiyya <abdulbasitsultan4@gmail.com>
2022-07-06 13:51:59 +0300

    filter reports modals

--- views/modals/portal/filter_report.blade.php
+++ views/modals/portal/filter_report.blade.php
@@ -5,7 +5,7 @@
 				<h5 class="modal-title" id="filterPortalReportTitle">Filter Report</h5>
 				<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
 			</div>
-			<div class="modal-body" id="reportPrint">
+			<div class="modal-body" id="generalReport">
 				
 			</div>
 		</div>
